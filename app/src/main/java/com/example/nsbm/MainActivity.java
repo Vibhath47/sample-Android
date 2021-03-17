e56288b986c8128cfd0539421d09450c0c04e6d1
Gaming <avbodiyabadu@students.nsbm.ac.lk>
2021-03-17 12:45:08 +0530

    initial commit

---
+++ app/src/main/java/com/example/nsbm/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.nsbm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn1);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
